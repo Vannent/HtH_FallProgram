17a8a81e4012381f022a5b2555323c76666e0c25
Roberto Cedeno <98601749+Vannent@users.noreply.github.com>
2022-10-11 12:32:20 -0700

    add rplt folder

---
+++ Getting-Started-with-Python-Vannent/main.py
@@ -0,0 +1,12 @@
+fullname = "Roberto Cedeno"
+nickname = "Enrique"
+age = 23
+python_knowledge = False
+hobbies = ["anime", "video games", "programming", "walks", "hiking"]
+favorite_things = {
+  "movie": "Mugen Train",
+  "food": "pasta",
+  "color": "gray",
+}
+
+all_vars = dict(vars())
